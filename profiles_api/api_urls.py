df25197fdc094e8080567e9bc842ea0de9fbd8c3
Rajath <rajathedgefinity@gmail.com>
2020-03-02 17:28:01 +0530

    Added Login API and Token Auth

--- profiles_api/api_urls.py
+++ profiles_api/api_urls.py
@@ -4,5 +4,6 @@ from profiles_api import api_routers
 
 urlpatterns = [
     path('v1apiview/',api_views.HelloApiView.as_view()),
+    path('login/', api_views.UserLoginApiView.as_view()),
     path('v1viewset/',include(api_routers.router.urls)),
 ]
