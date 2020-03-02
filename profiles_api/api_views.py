9677073a4341b7789cabc1b51c9f1455b471bca5
Rajath <rajathedgefinity@gmail.com>
2020-03-02 14:47:49 +0530

    Worked with APIView and Added All PUT, PATCH, GET and POST Methods

--- profiles_api/api_views.py
+++ profiles_api/api_views.py
@@ -1,9 +1,14 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from profiles_api import serializers
 
 
 class HelloApiView(APIView):
     """Test API View"""
+
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
         """Returns a list of APIView Features"""
@@ -14,3 +19,26 @@ class HelloApiView(APIView):
             'Is mapped manually to URLs',
         ]
         return Response({'messaage':'Hello!','an_apiview':an_apiview})
+
+    def post(self, request):
+        """Create a Hello Message with our Name"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message':message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk=None):
+        """Handle Updating an Object"""
+        return Response({'method':'PUT'})
+
+    def patch(self, request, pk=None):
+        """Handle Partial Update of an Object"""
+        return Response({'method':'PATCH'})
+
+    def delete(self, request, pk=None):
+        """Delete an Object"""
+        return Response({'method':'DELETE'})
